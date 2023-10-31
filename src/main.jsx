44e30b1b1983b3bc542827019071a32ca9251cd6
chandrakommi <chandrasekhar.kommi09@gmail.com>
2023-10-31 09:58:44 +0530

    use createRoot to render app

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import { store } from './redux/store.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootDiv = document.getElementById('root')
+const root = createRoot(rootDiv)
+
+root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
 )
